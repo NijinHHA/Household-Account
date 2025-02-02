16d72d2faece8a03137bac2b795e5e616d02b2fb
heejin131 <journyjean131@gmail.com>
2025-02-02 22:57:01 +0900

    cal_ver1

--- src/household_account/where_now.py
+++ src/household_account/where_now.py
@@ -1,11 +1,36 @@
 import typer
+import pandas as pd
 
 def hha():
-    return "Nice Spending"
+    return pd.DataFrame(data)
 
-def print_hha():
-    r = hha()
+data = {"날짜" : [1,2,3,4,5,6,7,8,9,10],
+        "지출" : [10000, 15000, 55000, 20000, 2000, 100000, 40000, 0, 10000, 20000],
+        "수입" : [0, 0, 0, 0, 0, 0, 0, 800000, 0, 0],
+        "카테고리" : ["식비", "식비", "교통비", "의료비", "택시비", "경조사비", "의류비", "생활비", "식비", "택시비"]}
+
+df = pd.DataFrame(data)
+
+#분석
+def analyze_budget(df):
+    total_expenses = df["지출"].sum()
+    total_income = df["수입"].sum()
+    balance = total_income - total_expenses
+    return f"===가계부===\n총 지출: {total_expenses:,} 원\n총 수입: {total_income:,} 원\n총 합계 금액(잔액): {balance:,} 원\n==================="
+
+# 메인작업함수
+def process_budget():
+    df = hha()
+
+    r = analyze_budget(df)
     print(r)
 
 def entry_point():
-    typer.run(print_hha)
+    typer.run(process_budget)
+
+
+
+
+
+
+
---
+++ tests/test_1.py
@@ -0,0 +1,9 @@
+import pytest
+from household_account.where_now import hha
+
+#def test_hha(): 
+
+#given
+#when
+#then
+
