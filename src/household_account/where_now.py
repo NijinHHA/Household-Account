eeac2d4f26ff9a72c5aec0180acfa291b37ad346
heejin131 <journyjean131@gmail.com>
2025-02-03 00:51:34 +0900

    cal_ver2

--- src/household_account/where_now.py
+++ src/household_account/where_now.py
@@ -1,36 +1,40 @@
 import typer
 import pandas as pd
 
-def hha():
-    return pd.DataFrame(data)
+# 안좋은 소비 기준
+bad_spending_money = 20000
+bad_categories = ['택시비', '기타']
 
-data = {"날짜" : [1,2,3,4,5,6,7,8,9,10],
-        "지출" : [10000, 15000, 55000, 20000, 2000, 100000, 40000, 0, 10000, 20000],
-        "수입" : [0, 0, 0, 0, 0, 0, 0, 800000, 0, 0],
-        "카테고리" : ["식비", "식비", "교통비", "의료비", "택시비", "경조사비", "의류비", "생활비", "식비", "택시비"]}
+# 가계부 데이터 프레임 생성
+columns = ["지출", "카테고리"]
+data = []
+df = pd.DataFrame(data, columns=columns)
 
-df = pd.DataFrame(data)
+
+def hha(expenses:int, category:str):
+    if expenses <= bad_spending_money:
+       print(f"{category}에 {expenses}를 썼습니다. 이는 효율적인 소비입니다.")
+    elif category in bad_categories:
+       print(f"{category}에 {expenses}원을 썼습니다. 이는 비효율적인 소비입니다.")
+    else:
+       print(f"그럴만할 일이 있었겠지..딱한것..")
+    
+    edf = pd.DataFrame([[expenses, category]], columns=["지출", "카테고리"])
+    return edf
 
 #분석
-def analyze_budget(df):
+def sum_budget(df):
     total_expenses = df["지출"].sum()
-    total_income = df["수입"].sum()
-    balance = total_income - total_expenses
-    return f"===가계부===\n총 지출: {total_expenses:,} 원\n총 수입: {total_income:,} 원\n총 합계 금액(잔액): {balance:,} 원\n==================="
-
+    return f"총 지출 : {total_expenses}원"
+ 
 # 메인작업함수
-def process_budget():
-    df = hha()
-
-    r = analyze_budget(df)
+def print_hha(expenses: int, category: str):
+    global df
+    new_entry = hha(expenses, category)  # 새로운 데이터 생성
+    df = pd.concat([df, new_entry], ignore_index=True)  # 기존 df에 추가    
+    r = sum_budget(df)
     print(r)
 
 def entry_point():
-    typer.run(process_budget)
-
-
-
-
-
-
+    typer.run(print_hha)
 
